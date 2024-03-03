a921f634d62039845bc526e338c687c4cd2d3e30
8-Lambda-8 <j.wasle111@gmail.com>
2024-03-03 14:17:58 +0100

    implemented button Matrix

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ uint8_t threshold = 16;
 void ADS_loop();
 bool checkChanged();
 
+void buttonMatrix_loop();
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -26,10 +28,16 @@ void setup() {
     ADSArray[i].setDataRate(4);
     for (size_t j = 0; j < 4; j++) ADSArray[i].requestADC(j);
   }
+
+  for (uint8_t i = 0; i < 4; i++) {
+    pinMode(buttonSensePins[i], INPUT);
+    pinMode(buttonPullPins[i], OUTPUT);
+  }
 }
 
 void loop() {
   ADS_loop();
+  buttonMatrix_loop();
 
   if (checkChanged()) {
     for (size_t i = 0; i < 8; i++) {
@@ -63,4 +71,26 @@ bool checkChanged() {
     if (abs(faderValuesLast[i] - faderValues[i]) > threshold) changed = true;
   }
   return changed;
+}
+
+void buttonMatrix_loop() {
+  memcpy(buttonArrayLast, buttonArray, sizeof(buttonArray));
+
+  for (size_t p = 0; p < sizeof(buttonPullPins); p++) {
+    for (uint8_t i = 0; i < sizeof(buttonPullPins); i++) digitalWrite(buttonPullPins[i], i != p);
+    delay(1);
+    for (size_t s = 0; s < sizeof(buttonSensePins); s++) {
+      buttonArray[p * sizeof(buttonSensePins) + s] = digitalRead(buttonSensePins[s]);
+    }
+  }
+
+  for (size_t i = 0; i < sizeof(buttonArray); i++) {
+    if (buttonArray[i] != buttonArrayLast[i]) {
+      uint8_t y = i / sizeof(buttonPullPins);
+      uint8_t x = i % sizeof(buttonPullPins);
+
+      Serial.printf("Button %2d (%d,%d) changed to %s\n", i, x, y,
+                    buttonArray[i] ? "Released" : "Pressed");
+    }
+  }
 }
